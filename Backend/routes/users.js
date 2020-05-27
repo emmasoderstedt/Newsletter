57d8cdd28e1c2cba921b5b7a171940e0c55a342f
Emma S <emma.soderstedt@gmail.com>
2020-05-27 15:42:06 +0200

    postuser

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('tjenis!');
+
+  fs.readFile('users.json', (err,data) => {
+    if(err) throw err;
+    var users = JSON.parse(data);
+    res.send(users);
+  })
+
 });
+
+router.post('/', function(req, res, next) {
+
+fs.readFile('users.json', (err, data) => {
+  if(err)
+  {
+    throw err;
+  }
+        
+  var users = JSON.parse(data);
+
+  newuser =    
+  {
+    "id": req.body.id,
+    "userName": req.body.userName,
+    "userEmail": req.body.userEmail
+  }
+
+  users.push(newuser);
+
+  var saveUsers= JSON.stringify(users, null, 2);
+
+  fs.writeFile('users.json', saveUsers, (err, data) => 
+  {
+    if(err) 
+    {
+      throw err;
+    }
+
+});
+
+res.send("Ny användare sparad!");
+})
+
+});
+
 
 module.exports = router;
